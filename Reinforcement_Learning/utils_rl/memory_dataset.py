514c907717f6c9bb42833bf2cee59b11e2061eba
francesco <varolif@student.ethz.ch>
2019-11-04 13:33:12 +0100

    replay memory for online learning

--- Reinforcement_Learning/utils_rl/memory_dataset.py
+++ Reinforcement_Learning/utils_rl/memory_dataset.py
@@ -1,15 +1,41 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
 
+
+class ReplayMemoryDataset(Dataset):
+    """
+    Fixed size Memory to aggregate datasets
+    memory_size: max number of datasets (iterations)
+    """
+    def __init__(self, memory_size):
+        self.data = []
+        self.rewards = []
+        self.max_size = memory_size
+
+    def add(self, dataset):
+        add_len = len(dataset)
+        my_len = len(self.data)
+        excess = my_len + add_len - self.max_size
+        if excess > 0:
+            del self.data[0:excess]
+        self.data += dataset.data
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def get_rewards(self, index):
+        return self.rewards[index]
+
+    def __len__(self):
+        return len(self.data)
+
 class MemoryDataset(Dataset):
     """
     Dataset to store memory
-        The memory is a list of transactions (tuples) from many trajectory collected during one iteration.
-        We transform it to a dataset where the data is a list of trajectories (each traj is a list of 2 tensor, x and y)
-        to check that the traj are correct plot the position over time, if doesn't make sense could be due to
-        multiple workers collecting data and appending trans. ot in order
+    The memory is a list of transactions (tuples) from many trajectory collected during one iteration.
+    We transform it to a dataset where the data is a list of trajectories (each traj is a list of 2 tensor, x and y)
     """
-    def __init__(self, memory_list, max_len=None):
+    def __init__(self, memory_list, device, dtype, max_len=None):
         self.data = []
         self.rewards = []
 
@@ -27,8 +53,8 @@ class MemoryDataset(Dataset):
             rew.append(transition.reward)
             len_traj += 1
             if transition.mask == 0:
-                unpadded_data.append([torch.tensor(trajectory_states),
-                                  torch.tensor(trajectory_actions), len_traj])
+                unpadded_data.append([torch.tensor(trajectory_states).to(dtype).to(device),
+                                  torch.tensor(trajectory_actions).to(dtype).to(device), len_traj])
                 self.rewards.append(rew)
                 len_traj = 0
                 trajectory_states = []
@@ -39,8 +65,8 @@ class MemoryDataset(Dataset):
             max_len = max(lengths)
 
         for unpad_traj in unpadded_data:
-            pad_x = torch.zeros([max_len, state_dim])
-            pad_y = torch.zeros([max_len, action_dim])
+            pad_x = torch.zeros([max_len, state_dim]).to(dtype).to(device)
+            pad_y = torch.zeros([max_len, action_dim]).to(dtype).to(device)
             pad_x[:unpad_traj[2], :] = unpad_traj[0]
             pad_y[:unpad_traj[2], :] = unpad_traj[1]
             self.data.append([pad_x, pad_y, unpad_traj[2]])
--- Reinforcement_Learning/utils_rl/replay_memory.py
+++ Reinforcement_Learning/utils_rl/replay_memory.py
@@ -30,9 +30,31 @@ class Memory(object):
         return len(self.memory)
 
 
-class IterMemory(object):
-    def __init__(self):
-        self.memory = [[]]
+class ReplayMemory(object):
+    def __init__(self, memory_size):
+        self.memory = []
+
+    def push(self, *args):
+        """Saves a transition."""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size=None):
+        if batch_size is None:
+            return Transition(*zip(*self.memory))
+        else:
+            random_batch = random.sample(self.memory, batch_size)
+            return Transition(*zip(*random_batch))
+
+    def append(self, new_memory):
+        self.memory += new_memory.memory
+
+    def __len__(self):
+        return len(self.memory)
+
+
+
+
+
 
     def push(self, *args):
         """Saves a transition."""

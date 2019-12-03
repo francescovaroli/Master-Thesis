7f6ee8aa3cee72e5c6e1331f8d9cd12ee5af5fb1
Francesco Varoli <varolif@eu-login-09.euler.ethz.ch>
2019-12-03 11:02:39 +0100

    allow fast iter

--- Reinforcement_Learning/NP_learning_mu_fixed_sigma.py
+++ Reinforcement_Learning/NP_learning_mu_fixed_sigma.py
@@ -23,15 +23,11 @@ parser.add_argument('--env-name', default="MountainCarContinuous-v0", metavar='G
 parser.add_argument('--render', action='store_true', default=False,
                     help='render the environment')
 
-parser.add_argument('--improve-mean', default=True,
-                    help='whether to use the improved mean or actions sampled from them as context points')
-parser.add_argument('--sample-improved-action', default=True,
-                    help='sample actions fro improved mean or improve old actions')
 parser.add_argument('--use-running-state', default=False,
                     help='store running mean and variance instead of states and actions')
 parser.add_argument('--max-kl', type=float, default=5e-2, metavar='G',
                     help='max kl value (default: 1e-2)')
-parser.add_argument('--num-ensembles', type=int, default=10, metavar='N',
+parser.add_argument('--num-ensembles', type=int, default=1, metavar='N',
                     help='episode to collect per iteration')
 parser.add_argument('--max-iter-num', type=int, default=501, metavar='N',
                     help='maximal number of main iterations (default: 500)')
@@ -39,20 +35,22 @@ parser.add_argument('--log-std', type=float, default=-1.0, metavar='G',
                     help='log std for the policy (default: -1.0)')
 parser.add_argument('--gamma', type=float, default=0.999, metavar='G',
                     help='discount factor (default: 0.99)')
+parser.add_argument("--init-normal", default=False,
+                    help='inititalize context point from the NP')
 
-parser.add_argument('--use-mean', default=False, metavar='N',
+parser.add_argument('--use-mean', default=True, metavar='N',
                     help='train & condit on improved means/actions'),
-parser.add_argument('--fixed-sigma', default=0.05, metavar='N',
+parser.add_argument('--fixed-sigma', default=None, metavar='G', type=float,
                     help='sigma of the policy')
-parser.add_argument('--epochs-per-iter', type=int, default=30, metavar='G',
+parser.add_argument('--epochs-per-iter', type=int, default=10, metavar='G',
                     help='training epochs of NP')
-parser.add_argument('--replay-memory-size', type=int, default=75, metavar='G',
+parser.add_argument('--replay-memory-size', type=int, default=20, metavar='G',
                     help='size of training set in episodes')
 parser.add_argument('--z-dim', type=int, default=128, metavar='N',
                     help='dimension of latent variable in np')
-parser.add_argument('--r-dim', type=int, default=256, metavar='N',
+parser.add_argument('--r-dim', type=int, default=128, metavar='N',
                     help='dimension of represenation space in np')
-parser.add_argument('--h-dim', type=int, default=256, metavar='N',
+parser.add_argument('--h-dim', type=int, default=128, metavar='N',
                     help='dimension of hidden layers in np')
 parser.add_argument('--np-batch-size', type=int, default=8, metavar='N',
                     help='batch size for np training')
@@ -86,16 +84,21 @@ parser.add_argument('--gpu-index', type=int, default=0, metavar='N')
 
 parser.add_argument('--use-attentive-np', default=True, metavar='N',
                      help='use attention in policy and value NPs')
-parser.add_argument('--episode-specific-value', default=False, metavar='N',
+parser.add_argument('--episode-specific-value', default=True, metavar='N',
                     help='condition the value np on all episodes')
-
+parser.add_argument("--plot-every", type=int, default=1,
+                    help='plot every n iter')
+parser.add_argument("--num-testing-points", type=int, default=1,
+                    help='how many point to use as only testing during NP training')
 args = parser.parse_args()
 
 initial_training = True
 
-np_spec = '_{}z_{}rm_{}e_imprM:{}_sampled_a:{}'.format(args.z_dim, args.replay_memory_size,
-                                                       args.epochs_per_iter, args.improve_mean,
-                                                       args.sample_improved_action)
+max_episode_len = 999
+num_context_points = max_episode_len - args.num_testing_points
+
+np_spec = '_{}z_{}rm_{}e_num_context:{}'.format(args.z_dim, args.replay_memory_size,
+                                                       args.epochs_per_iter, num_context_points)
 run_id = '/Value_NP_mean:{}_A:{}_fixSTD:{}_epV:{}_init_tr:{}_{}ep_{}kl_{}gamma_'.format(args.use_mean,
                                                 args.use_attentive_np, args.fixed_sigma, args.episode_specific_value,
                                                 initial_training, args.num_ensembles, args.max_kl, args.gamma) + np_spec
@@ -104,7 +107,6 @@ args.directory_path += run_id
 torch.set_default_dtype(args.dtype)
 
 """environment"""
-max_episode_len = 999
 env = gym.make(args.env_name)
 state_dim = env.observation_space.shape[0]
 action_dim = env.action_space.shape[0]
@@ -123,15 +125,15 @@ env.seed(args.seed)
 '''create neural process'''
 if args.use_attentive_np:
     policy_np = AttentiveNeuralProcess(state_dim, action_dim, args.r_dim, args.z_dim, args.h_dim,
-                                                       args.z_dim, use_self_att=True).to(args.device_np)
+                                                       args.z_dim, use_self_att=True, fixed_sigma=args.fixed_sigma).to(args.device_np)
 else:
     policy_np = NeuralProcess(state_dim, action_dim, args.r_dim, args.z_dim, args.h_dim,
                               fixed_sigma=args.fixed_sigma).to(args.device_np)
 
 optimizer = torch.optim.Adam(policy_np.parameters(), lr=3e-4)
 np_trainer = NeuralProcessTrainerRL(args.device_np, policy_np, optimizer,
-                                    num_context_range=(400, 500),
-                                    num_extra_target_range=(400, 500),
+                                    num_context_range=(num_context_points, num_context_points),
+                                    num_extra_target_range=(args.num_testing_points, args.num_testing_points),
                                     print_freq=50)
 
 if args.use_attentive_np:
@@ -141,8 +143,8 @@ else:
     value_np = NeuralProcess(state_dim, 1, args.v_r_dim, args.v_z_dim, args.v_h_dim).to(args.device_np)
 value_optimizer = torch.optim.Adam(value_np.parameters(), lr=3e-4)
 value_np_trainer = NeuralProcessTrainerRL(args.device_np, value_np, value_optimizer,
-                                          num_context_range=(400, 500),
-                                          num_extra_target_range=(400, 500),
+                                          num_context_range=(num_context_points, num_context_points),
+                                          num_extra_target_range=(args.num_testing_points, args.num_testing_points),
                                           print_freq=50)
 """create replay memory"""
 replay_memory = ReplayMemoryDataset(args.replay_memory_size)
@@ -150,7 +152,6 @@ value_replay_memory = ValueReplay(args.v_replay_memory_size)
 
 """create agent"""
 agent = Agent(env, policy_np, args.device_np, running_state=running_state, render=args.render, attention=args.use_attentive_np)
-
 
 def estimate_eta_2(actions, means, stddevs, disc_rews):
     d = actions.shape[-1]
@@ -252,7 +253,8 @@ def estimate_disc_rew(all_episodes, i_iter, episode_specific_value=False):
                 estimated_disc_rew.append(r_est.view(-1).numpy())
                 value_stddevs.append(values_distr.stddev.view(-1).numpy())
             all_values.append(values)
-    plot_NP_value(value_np, all_states, all_values, all_episodes, all_rewards, env, args, i_iter)
+    if i_iter % args.plot_every == 0:
+        plot_NP_value(value_np, all_states, all_values, all_episodes, all_rewards, env, args, i_iter)
     return estimated_disc_rew, value_stddevs
 
 def sample_initial_context_uniform(num_episodes):
@@ -286,7 +288,10 @@ def sample_initial_context_normal(num_episodes):
         for i in range(max_episode_len):
             states[:, i, :] = torch.from_numpy(env.observation_space.sample())
 
-        z_sample = z_init.sample([1, args.z_dim])
+        if args.use_attentive_np:
+            z_sample = z_init.sample([1, args.z_dim+args.z_dim])
+        else:
+            z_sample = z_init.sample([1, args.z_dim])
         z_sample = z_sample.unsqueeze(1).repeat(1, max_episode_len, 1)
         means_init, stds_init = policy_np.xz_to_y(states, z_sample)
         actions_init = Normal(means_init, stds_init).sample()
@@ -320,7 +325,10 @@ def main_loop():
     for i in range(num_episodes):
         colors.append('#%06X' % randint(0, 0xFFFFFF))
     #print('sampling initial context')
-    improved_context_list = sample_initial_context_uniform(args.num_ensembles)
+    if args.init_normal:
+        improved_context_list = sample_initial_context_normal(args.num_ensembles)
+    else:
+        improved_context_list = sample_initial_context_uniform(args.num_ensembles)
     plot_initial_context(improved_context_list, colors, env, args, '00')
     if initial_training:
         train_on_initial(improved_context_list)
@@ -346,14 +354,15 @@ def main_loop():
         t1 = time.time()
         #plot_initial_context(improved_context_list, colors, env, args, i_iter)
         # plot improved context and actions' discounted rewards
-        plot_improvements(complete_dataset, estimated_disc_rew, env, i_iter, args, colors)
+        if i_iter % args.plot_every == 0:
+            plot_improvements(complete_dataset, estimated_disc_rew, env, i_iter, args, colors)
 
         # create training set
         replay_memory.add(complete_dataset)
         train_np(replay_memory)
         #plot_training_set(i_iter, replay_memory, env, args)
-
-        plot_NP_policy(policy_np, improved_context_list, i_iter, log['avg_reward'], env, args, colors)
+        if i_iter % args.plot_every == 0:
+            plot_NP_policy(policy_np, improved_context_list, i_iter, log['avg_reward'], env, args, colors)
 
         avg_rewards.append(log['avg_reward'])
         if i_iter % args.log_interval == 0 and False:
@@ -368,5 +377,7 @@ def main_loop():
 
 create_directories(args.directory_path)
 main_loop()
+
+
 
 

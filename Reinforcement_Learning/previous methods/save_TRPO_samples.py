2602e2bd8ee6bdf4cd497451df10a4d4c13ad882
francesco <varolif@student.ethz.ch>
2019-12-18 11:44:05 +0100

    moved old methods

--- Reinforcement_Learning/previous methods/save_TRPO_samples.py
+++ Reinforcement_Learning/previous methods/save_TRPO_samples.py
@@ -24,7 +24,7 @@ parser.add_argument('--render', action='store_true', default=False,
                     help='render the environment')
 parser.add_argument('--log-std', type=float, default=-1.0, metavar='G',
                     help='log std for the policy (default: -1.0)')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+parser.add_argument('--gamma', type=float, default=0.999, metavar='G',
                     help='discount factor (default: 0.99)')
 parser.add_argument('--tau', type=float, default=0.95, metavar='G',
                     help='gae (default: 0.95)')
@@ -34,13 +34,13 @@ parser.add_argument('--max-kl', type=float, default=1e-2, metavar='G',
                     help='max kl value (default: 1e-2)')
 parser.add_argument('--damping', type=float, default=1e-2, metavar='G',
                     help='damping (default: 1e-2)')
-parser.add_argument('--num-threads', type=int, default=8, metavar='N',
+parser.add_argument('--num-threads', type=int, default=4, metavar='N',
                     help='number of threads for agent (default: 4)')
-parser.add_argument('--seed', type=int, default=1, metavar='N',
+parser.add_argument('--seed', type=int, default=13, metavar='N',
                     help='random seed (default: 1)')
-parser.add_argument('--min-batch-size', type=int, default=7990, metavar='N',
+parser.add_argument('--min-batch-size', type=int, default=5000, metavar='N',
                     help='minimal batch size per TRPO update (default: 2048)')
-parser.add_argument('--max-iter-num', type=int, default=501, metavar='N',
+parser.add_argument('--max-iter-num', type=int, default=1000, metavar='N',
                     help='maximal number of main iterations (default: 500)')
 parser.add_argument('--log-interval', type=int, default=1, metavar='N',
                     help='interval between training status logs (default: 10)')
@@ -54,12 +54,15 @@ torch.set_default_dtype(dtype)
 device = torch.device('cuda', index=args.gpu_index) if torch.cuda.is_available() else torch.device('cpu')
 if torch.cuda.is_available():
     torch.cuda.set_device(args.gpu_index)
-
+use_running_state = False
 """environment"""
 env = gym.make(args.env_name)
 state_dim = env.observation_space.shape[0]
 is_disc_action = len(env.action_space.shape) == 0
-running_state = ZFilter((state_dim,), clip=5)  # running list of states that allows to access precise mean and std
+if use_running_state:
+    running_state = ZFilter((state_dim,), clip=5)  # running list of states that allows to access precise mean and std
+else:
+    running_state = None
 # running_reward = ZFilter((1,), demean=False, clip=10)
 
 """seeding"""
@@ -98,10 +101,10 @@ def update_params(batch):
     """perform TRPO update"""
     trpo_step(policy_net, value_net, states, actions, returns, advantages, args.max_kl, args.damping, args.l2_reg)
 
-run_id = '(test)'
-directory_path = '/home/francesco/PycharmProjects/MasterThesis/RL memories/TRPO policies&samples MCC' + run_id
+run_id = 'deb'
+directory_path = '/home/francesco/PycharmProjects/MasterThesis/value learning/' + run_id
 
-def plot_policy(policy_net, info):
+def plot_policy(net, info):
     from mpl_toolkits.mplot3d import Axes3D
     # Axes3D import has side effects, it enables using projection='3d' in add_subplot
     import matplotlib.pyplot as plt
@@ -115,26 +118,33 @@ def plot_policy(policy_net, info):
     for _x1 in x1:
         for _x2 in x2:
             state = tensor([_x1, _x2], device=device).unsqueeze(0)
-            zs.append(policy_net(state)[0][0].item())
+            zs.append(net(state)[0][0].item())
     Z = (np.array(zs)).reshape(X1.shape).transpose()
 
     ax = plt.axes(projection='3d')
-    ax.plot_surface(X1, X2, Z, cmap='viridis',  vmin=-1., vmax=1.)
-
     ax.set_xlabel('Position')
     ax.set_ylabel('Velocity')
-    ax.set_zlabel('Acceleration')
-    ax.set_zlim(env.action_space.low, env.action_space.high)
-    name = 'TRPO on {} iter: {} avg_rew: {}'.format(args.env_name, info[0], int(info[1]))
+    if info[2] == 'policies':
+        ax.set_zlabel('Acceleration')
+        ax.set_zlim(env.action_space.low, env.action_space.high)
+        ax.plot_surface(X1, X2, Z, cmap='viridis',  vmin=-1., vmax=1.)  #
+
+    else:
+        ax.set_zlabel('Value')
+        ax.set_zlim(-50, 100)
+        ax.plot_surface(X1, X2, Z, cmap='viridis', vmin=-50., vmax=100.)  # ,
+
+    name = 'TRPO {} iter: {} avg_rew: {}'.format(info[2], info[0], int(info[1]))
     ax.set_title(name, pad=20)
-    fig.savefig(directory_path+'/policies/'+name)
+    fig.savefig(directory_path+'/'+info[2]+'/'+name)
     plt.close(fig)
     # plt.show()
 
 def create_directories(directory_path):
     os.mkdir(directory_path)
     os.mkdir(directory_path+'/policies/')
-    os.mkdir(directory_path+'/episodes/')
+    os.mkdir(directory_path+'/values/')
+    #os.mkdir(directory_path+'/episodes/')
 
 
 def create_memory_file(i_iter):
@@ -146,7 +156,7 @@ def create_memory_file(i_iter):
 def main_loop():
     for i_iter in range(args.max_iter_num):
         # (1)
-        memory_file = create_memory_file(i_iter)
+        #memory_file = create_memory_file(i_iter)
 
         # generate multiple trajectories that reach the minimum batch_size
         batch, log, memory = agent.collect_samples(args.min_batch_size)  # batch of at least batch_siz transitions from multiple
@@ -160,7 +170,8 @@ def main_loop():
         if i_iter % args.log_interval == 0:
             print('{}\tT_sample {:.4f}\tT_update {:.4f}\tR_min {:.2f}\tR_max {:.2f}\tR_avg {:.2f}'.format(
                 i_iter, log['sample_time'], t1-t0, log['min_reward'], log['max_reward'], log['avg_reward']))
-            plot_policy(policy_net, (i_iter, log['avg_reward']))
+            plot_policy(policy_net, (i_iter, log['avg_reward'], 'policies'))
+            plot_policy(value_net, (i_iter, log['avg_reward'], 'values'))
 
 
         if args.save_model_interval > 0 and (i_iter+1) % args.save_model_interval == 0:
@@ -169,12 +180,9 @@ def main_loop():
                         open(os.path.join(assets_dir(), 'learned_models/{}_trpo.p'.format(args.env_name)), 'wb'))
             to_device(device, policy_net, value_net)
 
-        #  Saving samples from this iteration as file
-        with open(memory_file, 'wb') as file_m:
-            pickle.dump(memory, file_m, protocol=pickle.HIGHEST_PROTOCOL)
-
-    """clean up gpu memory"""
-    torch.cuda.empty_cache()
+            #  Saving samples from this iteration as file
+            with open(memory_file, 'wb') as file_m:
+                pickle.dump(memory, file_m, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 create_directories(directory_path)

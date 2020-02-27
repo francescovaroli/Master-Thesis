4fd88bdc920370db2875eb2088e05fa7d8249e46
francesco <varolif@student.ethz.ch>
2020-02-27 11:20:16 +0100

    running experiment np_hopper3

--- Reinforcement_Learning/gather_results.py
+++ Reinforcement_Learning/gather_results.py
@@ -11,12 +11,12 @@ def list_folders(dir):
     return subfold
 
 
-folder_path = '/media/francesco/Irene/Francesco/Master Thesis/scratch/np_hopper'
+folder_path = '/media/francesco/Irene/Francesco/Master Thesis/scratch/swimmer_mi_2/mi_swimmer_2/'
 all_folders = list_folders(folder_path)
 num_folders = len(all_folders)
 alpha = 2.5/num_folders
 max_len = 1000000
-chunk_size = 5000
+chunk_size = 10000
 num_seeds = 3
 
 fig_rew, ax_rew = plt.subplots(1, 1)
@@ -24,31 +24,45 @@ ax_rew.set_xlabel('number of steps')
 ax_rew.set_ylabel('average reward')
 title = 'Average Reward History'
 ax_rew.set_title(title)
+if 'NP' in all_folders[1]:
+    label = 'Attentive Neural Process'
+    color = 'b'
+elif 'MI' in all_folders[1]:
+    label = 'Mean Kearnel Interpolation'
+    color = 'g'
+elif 'MLP' in all_folders[1]:
+    label = 'Multi Layer Perceptron'
+    color = 'magenta'
 
 for s, subfolder_path in enumerate(all_folders[1:]):
     print('...', s)
-    data = []
+    step_data = []
+    rew_data = []
+    lens = []
     for i in range(num_seeds):
-        file_path = subfolder_path + '/{}.csv'.format(i)
-        data.append(np.genfromtxt(file_path))
-
-    min_len = min(min(len(l) for l in data), max_len)
-    data_avg = np.zeros(min_len)
-    for d in data:
-        data_avg += d[:min_len]/num_seeds
+        file_path = subfolder_path + '/avg{}.csv'.format(i)
+        data = np.genfromtxt(file_path, delimiter=',')
+        step_data.extend(data[:, 0])
+        rew_data.extend(data[:, 1])
+        lens.append(data[-1, 0])
+    min_len = min(min(lens), max_len)
 
     start = 0
     avg_rews = []
+    steps = []
     while start < min_len:
         end = min(start + chunk_size, min_len)
-        chunk = data_avg[start:end]
+        indexes = [i for i in range(len(step_data)) if start < step_data[i] <= end]
+        chunk = np.take(rew_data, indexes)
+        start = end
+        if len(chunk) == 0:
+            print('missing ', start, end)
+            continue
         avg = sum(chunk)/len(chunk)
         avg_rews.append(avg)
-        start = end
-    #label = 'Multi Layer Perceptron'
-    #label = 'Mean Kearnel Interpolation'
-    label = 'Attentive Neural Process'
-    ax_rew.plot(np.arange(1, len(avg_rews)+1)*chunk_size, avg_rews, alpha=0.6, c='b', label=label)
+        steps.append(end)
+
+    ax_rew.plot(np.arange(1, len(avg_rews)+1)*chunk_size, avg_rews, alpha=alpha, c=color, label=label)
 
     if s == 0: plt.legend()
 plt.grid()

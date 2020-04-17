1d83b2adcfac64825dc23c6aa1f723bb2faf9ee0
francesco <varolif@student.ethz.ch>
2020-04-17 11:54:34 +0200

    running experiment mi_walk_hop

--- Reinforcement_Learning/gather_results.py
+++ Reinforcement_Learning/gather_results.py
@@ -10,8 +10,8 @@ def list_folders(dir):
         subfold.append(folder[0])
     return subfold
 
-for sub in ['walk', 'mount', 'chee']:
-    folder_path = '/media/francesco/Irene/Francesco/Master Thesis/scratch/rm_ablation/'+sub
+for sub in ['walk', 'swim', 'hop', 'chee']:
+    folder_path = '/media/francesco/Irene/Francesco/Master Thesis/scratch/mi_walk_hop/'+sub
     all_folders = list_folders(folder_path)
     num_folders = len(all_folders)
     alpha = 0.6
@@ -75,15 +75,15 @@ for sub in ['walk', 'mount', 'chee']:
         else:
             color='b'
             label = 'ER size: 120 ep'''
-        if 'D:0,5' in subfolder_path:
+        if '0,6kl' in subfolder_path:
             color = 'r'
-            label = '0,5 stddev'
+            label = '0,6 kl'
         elif '0,04kl' in subfolder_path:
             color = 'b'
             label = '0,04 kl'
         else:
             color = 'g'
-            label = '0,4 stddev'
+            label = '1,5 kl'
         ax_rew.plot(np.arange(1, len(avg_rews)+1)*chunk_size, avg_rews, alpha=alpha, c=color, label=label)
 
         handles, labels = plt.gca().get_legend_handles_labels()
